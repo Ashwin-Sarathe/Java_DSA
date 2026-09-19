9db3b8333987dc361f9d6d2981091c30a78b1961
ashwinsarathe <ashwinsarathe01@gmail.com>
2026-09-19 09:33:07 +0530

    Added PascalTriangle3.java

---
+++ Arrays/PascalTriangle3.java
@@ -0,0 +1,23 @@
+//Print whole Pascal Triangle till the given row
+import java.util.*;
+class Solution {
+    static List<Integer> generateRows(int rowInd) {
+        int ans = 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(ans);
+        for (int i = 1; i < rowInd; i++) {
+            ans = ans * (rowInd - i);
+            ans = ans / i;
+            list.add(ans);
+        }
+        return list;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            list.add(generateRows(i));
+        }
+        return list;
+    }
+}
